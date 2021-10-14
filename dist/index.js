c69940653a7f78d8c295c2be69f6079a7f6d1669
Julio-Freitas <julio.clf31@gmail.com>
2021-10-13 22:50:05 -0300

    feat(title): implements title and effect when to show text

--- dist/index.js
+++ dist/index.js
@@ -5,6 +5,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const listpokemonView_1 = __importDefault(require("./view/listpokemonView"));
 const elPokemon = document.querySelector('[data-js="listPokemon"]');
+const elTitle = document.querySelector('h1.title');
+const effectCss = (text, el, speed = 100) => {
+    const textToDigited = text;
+    const splitText = textToDigited === null || textToDigited === void 0 ? void 0 : textToDigited.split('');
+    el.innerHTML = ' ';
+    splitText.forEach((chart, index) => {
+        setTimeout(() => {
+            el.innerHTML += `${chart}`;
+        }, speed * index);
+    });
+};
 const $html = (content) => {
     if (elPokemon) {
         elPokemon.innerHTML = content;
@@ -13,7 +24,8 @@ const $html = (content) => {
 const initialize = async () => {
     const listResult = await (0, listpokemonView_1.default)();
     $html('<h1 class="loading">Carregando o conteudo...</h1>');
-    setTimeout(() => $html(listResult), 1000);
+    effectCss('Lista de Pokemon', elTitle, 150);
+    setTimeout(() => $html(listResult), 150);
 };
 initialize();
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,18 @@ import listpokemonView from './view/listpokemonView';
 const elPokemon = document.querySelector(
   '[data-js="listPokemon"]'
 ) as HTMLElement;
+const elTitle = document.querySelector('h1.title') as HTMLTitleElement;
+
+const effectCss = (text: string, el: HTMLTitleElement, speed = 100) => {
+  const textToDigited = text;
+  const splitText = textToDigited?.split('');
+  el.innerHTML = ' ';
+  splitText.forEach((chart, index) => {
+    setTimeout(() => {
+      el.innerHTML += `${chart}`;
+    }, speed * index);
+  });
+};
 
 const $html = (content: string): void => {
   if (elPokemon) {
@@ -13,8 +25,9 @@ const initialize = async () => {
   const listResult = await listpokemonView();
 
   $html('<h1 class="loading">Carregando o conteudo...</h1>');
+  effectCss('Lista de Pokemon', elTitle, 150);
 
-  setTimeout((): void => $html(listResult), 1000);
+  setTimeout((): void => $html(listResult), 150);
 };
 
 initialize();
